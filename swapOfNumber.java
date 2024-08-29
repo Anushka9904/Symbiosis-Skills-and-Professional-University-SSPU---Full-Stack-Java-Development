906c99fb232370fca0723a7f49e6af62150094ee
Anushka9904 <anushka99shinde@gmail.com>
2024-08-29 22:18:00 +0530

    Add files via upload

---
+++ swapOfNumber.java
@@ -0,0 +1,25 @@
+//write a java program to swap two numbers
+package Assignments;
+
+import java.util.Scanner;
+
+public class swapOfNumber {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the first number: ");
+        int num1 = sc.nextInt();
+        System.out.print("Enter the second number: ");
+        int num2 = sc.nextInt();
+         // Swap using a temporary variabl
+        int temp = num1;
+        num1 = num2;
+        num2 = temp;
+        // Display the swapped numbers
+        System.out.println("After swapping:");
+        System.out.println("First number: " + num1);
+        System.out.println("Second number: " + num2);
+        sc.close();
+    }
+}
+
