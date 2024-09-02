cd2a776ba594a4dc4b6783ba9ffdf615b89eef01
Anushka9904 <anushka99shinde@gmail.com>
2024-09-02 23:31:56 +0530

    Add files via upload

---
+++ SquareRoot.java
@@ -0,0 +1,14 @@
+package Assignments;
+import java.util.Scanner;
+public class SquareRoot {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	        Scanner sc = new Scanner(System.in);
+	        System.out.print("Enter a number: ");
+	        double num = sc.nextDouble();
+	        double squareroot = Math.sqrt(num);
+	        System.out.println("The square root of " + num + " is:" + squareroot);
+	    }
+	}
+
+
