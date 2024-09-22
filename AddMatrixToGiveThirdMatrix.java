08c5d9948831545c0b5d1c2c4652cbad741d2377
Anushka9904 <anushka99shinde@gmail.com>
2024-09-22 19:51:25 +0530

    Add files via upload

---
+++ AddMatrixToGiveThirdMatrix.java
@@ -0,0 +1,44 @@
+package Assignments;
+import java.util.Scanner;
+public class AddMatrixToGiveThirdMatrix {
+ public static void main(String[] args) {
+	         Scanner sc = new Scanner(System.in);
+	         System.out.print("Enter number of rows: ");
+	         int row = sc.nextInt();
+	         System.out.print("Enter number of columns: ");
+	         int col = sc.nextInt();
+	         
+	         int[][] matrixA = new int[row][col];
+	         int[][] matrixB = new int[row][col];
+	         int[][] sumMatrix = new int[row][col];
+	         System.out.println("Enter elements of first matrix:");
+	         fillMatrix(matrixA, sc);
+	         System.out.println("Enter elements of  second matrix:");
+	         fillMatrix(matrixB, sc);
+	         for (int i = 0; i < row; i++) {
+	         for (int j = 0; j < col; j++) {
+	         sumMatrix[i][j] = matrixA[i][j] + matrixB[i][j];
+	             }
+	         	}
+	         System.out.println("Sum of	two matrices:");
+	         printMatrix(sumMatrix);
+ 			}
+	     private static void fillMatrix(int[][] matrix, Scanner sc) {
+	     for (int i=0;i<matrix.length;i++) {
+	     for (int j=0;j<matrix[i].length;j++) {
+	     System.out.print("Element ["+i+"]["+j+"]:");
+	     matrix[i][j] = sc.nextInt();
+	             }
+	         	}
+	     		}
+	     private static void printMatrix(int[][] matrix) {
+	     for (int[] row : matrix) {
+	     for (int element : row) {
+	      System.out.println(element);
+	        }
+	        }
+	     	}
+	 }
+	
+
+
